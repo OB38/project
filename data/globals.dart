e693bf9f19618546e18639798f3636bb7a775a62
OB38 <114050480+OB38@users.noreply.github.com>
2024-05-27 22:51:52 +0300

    Create globals.dart

---
+++ data/globals.dart
@@ -0,0 +1,4 @@
+library habit_tracker.globals;
+//Saraksts ar izvēles kategorijām
+List<String> categoryOptions = ['Select a category', 'Intelligence', 'Spirit', 'Strength', 'Energy'];
+String selectedCategory = categoryOptions[0];
